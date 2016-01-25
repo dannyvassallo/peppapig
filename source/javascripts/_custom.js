c06c65b7b6370324bfea64779c7c428ecb50ea62
dannyvassallo <danielvassallo87@gmail.com>
2016-01-25 16:37:30 -0500

    added tour styles and csv parser for dates

--- source/javascripts/_custom.js
+++ source/javascripts/_custom.js
@@ -1,2 +1,24 @@
-// ADD YOUR JS HERE
-console.log('loaded');
+$(document).ready(function() {
+  $.ajax({
+    type: "GET",
+    url: "dates.txt",
+    dataType: "text",
+    success: function(data){
+      dateHandler(data);
+    }
+  });
+});
+
+
+
+function dateHandler(data){
+    var parsed = $.csv.toObjects(data);
+    $(parsed).each(function(){
+      var tourDate = this.date;
+      var venue = this.venue;
+      var href = this.link;
+      var dateConstructor = "<li class=\"collection-item cyan white-text ticket-links\"><div><p class=\"date\">"+tourDate+"</p> <p class=\"date-divider\">-</p> <p class=\"venue\">"+venue+"</p><a target=\"_blank\" href=\""+href+"\" class=\"secondary-content btn btn-small white\"><i class=\"fa fa-ticket cyan-text\"></i></a></div></li>";
+      console.log(dateConstructor);
+      $('#tour-dates').append(dateConstructor);
+    });
+}
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,5 @@
 //= require jquery
 //= require materialize-sprockets
 //= require _init-materialize
+//= require _jquery.csv-0.71
 //= require _custom
