56e5dcc3dc450aa5d20e00eb43edd7c5d773b346
dannyvassallo <danielvassallo87@gmail.com>
2016-01-25 17:18:04 -0500

    setup mock form

--- source/javascripts/_custom.js
+++ source/javascripts/_custom.js
@@ -1,3 +1,4 @@
+// GET DATES FROM TEXT FILE
 $(document).ready(function() {
   $.ajax({
     type: "GET",
@@ -8,17 +9,22 @@ $(document).ready(function() {
     }
   });
 });
-
-
-
+// CONSTRUCT HTML DATE TABLE FROM PARSED TEXT FILE
 function dateHandler(data){
-    var parsed = $.csv.toObjects(data);
-    $(parsed).each(function(){
-      var tourDate = this.date;
-      var venue = this.venue;
-      var href = this.link;
-      var dateConstructor = "<li class=\"collection-item cyan white-text ticket-links\"><div><p class=\"date\">"+tourDate+"</p> <p class=\"date-divider\">-</p> <p class=\"venue\">"+venue+"</p><a target=\"_blank\" href=\""+href+"\" class=\"secondary-content btn btn-small white\"><i class=\"fa fa-ticket cyan-text\"></i></a></div></li>";
-      console.log(dateConstructor);
-      $('#tour-dates').append(dateConstructor);
-    });
+  var parsed = $.csv.toObjects(data);
+  $(parsed).each(function(){
+    var tourDate = this.date;
+    var venue = this.venue;
+    var href = this.link;
+    var dateConstructor = "<li class=\"collection-item cyan white-text ticket-links\"><div><p class=\"date\">"+tourDate+"</p> <p class=\"date-divider\">-</p> <p class=\"venue\">"+venue+"</p><a target=\"_blank\" href=\""+href+"\" class=\"secondary-content btn btn-small white\"><i class=\"fa fa-ticket cyan-text\"></i></a></div></li>";
+    $('#tour-dates').append(dateConstructor);
+  });
 }
+
+$('input').focus(function(){
+  var query = $(this).next('label');
+  query.addClass('active');
+}).blur(function(){
+  var query = $(this).next('label');
+  query.removeClass('active');
+});
